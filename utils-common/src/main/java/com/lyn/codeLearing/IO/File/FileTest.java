b85c914c4641cc318016b30ea957159e50747901
wrx <wrx_lyn@126.com>
2018-05-06 21:39:14 +0800

    递归case

--- utils-common/src/main/java/com/lyn/codeLearing/IO/File/FileTest.java
+++ utils-common/src/main/java/com/lyn/codeLearing/IO/File/FileTest.java
@@ -1,4 +1,37 @@
 package com.lyn.codeLearing.IO.File;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class FileTest {
+
+    public static void main(String[] args) {
+
+        File file =new File("D:\\视频学习\\圣思源小\\Java SE 第八十七讲 File类详解及使用陷阱深度剖析");
+        /**
+         * 只返回后缀为pdf的文件名称
+         */
+        String[] names=file.list(new FilenameFilter() {
+            /**
+             * @param dir 当前这个file的目录
+             * @param name 文件名称
+             * @return
+             */
+            @Override
+            public boolean accept(File dir, String name) {
+                if (name.endsWith(".pdf")){
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        for (String name:names
+             ) {
+            System.out.println(name);
+        }
+
+
+    }
+
 }
--- utils-common/src/main/java/com/lyn/codeLearing/Recursion/Fab.java
+++ utils-common/src/main/java/com/lyn/codeLearing/Recursion/Fab.java
@@ -1,4 +1,36 @@
 package com.lyn.codeLearing.Recursion;
 
+import com.lyn.utils.FileUtils;
+
+import java.io.File;
+
+/**
+ * 斐波那契数列
+ * 1 1 2 3 5 8 .....
+ */
 public class Fab {
+
+    /**
+     * n代表第几个数，10就是第十个数
+     * @param n
+     * @return
+     */
+    public static int compute(int n){
+        //递归出口
+        if(1==n || 2==n){
+            return 1;
+        }
+        else{
+            return compute(n-1) +compute(n-2);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(compute(5));
+
+        File file =new File("d:\\dd");
+        FileUtils.deleteAllFiles(file);
+    }
 }
